df7ecd85398e079ac628951cada08b607087987f
YunTaeHyeon <t05h19@naver.com>
2024-01-26 12:35:07 +0900

    멤버 도메인 생성 && 회원가입 로직

---
+++ src/main/java/com/example/demo/member/MemberApp.java
@@ -0,0 +1,15 @@
+package com.example.demo.member;
+
+/*
+public class MemberApp {
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+        Member findMember = memberService.findMember(1L);
+        System.out.println("new member = " + member.getName());
+        System.out.println("find Member = " + findMember.getName());
+    }
+}
+*/
+//이렇게 하지 말라는 뜻입니다아아ㅏㅏ 애플리케이션 로직으로 테스트하면 안좋음
